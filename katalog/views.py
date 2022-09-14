777fbed5276225afdfec130e45dc9daa13dc5d75
triviaspace <shafatriviaa@gmail.com>
2022-09-14 23:43:55 +0700

    do the task 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+
+# TODO : Create your views here.
+def katalog_view(request):
+    daftar_item = CatalogItem.objects.all()
+    context = {
+        'list_item': daftar_item,
+        'Nama': 'Shafa Trivia Ezananda',
+        'ID': '2206026971'
+    }
+    return render(request, "katalog.html", context)
